75a6d6a1205e114d87a9d58578ba313e69410db9
BojanStipic <bojanstipic@gmail.com>
2018-07-08 20:23:18 +0200

    Add `cenovnik` service

---
+++ business/src/main/resources/static/core/cenovnik/cenovnik.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('core.cenovnik')
+	.service('CenovnikService', function($http) {
+		let prefix = 'http://localhost:8080/api/cenovnik';
+
+		this.getAll = () => {
+			return $http.get(`${prefix}/`);
+		};
+		this.getOne = (id) => {
+			return $http.get(`${prefix}/${id}`);
+		};
+		this.getStavke = (id) => {
+			return $http.get(`${prefix}/stavke/${id}`);
+		};
+	});
